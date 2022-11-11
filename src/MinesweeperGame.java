5dde9c16c4c22a4ed7a8aa18fbf500a24693f24b
Maciej Chodacki <maciek.chodacki@wp.pl>
2022-11-12 00:32:22 +0100

    Second commit, press button override method added, displaying results of mouse click on cells

--- src/MinesweeperGame.java
+++ src/MinesweeperGame.java
@@ -3,7 +3,10 @@ import com.codegym.engine.cell.Game;
 
 public class MinesweeperGame extends Game{
 private final int DIMENSION = 12;
-public CellProperties[][] cellProperties = new CellProperties[DIMENSION][DIMENSION];
+public CellProperties[][] cellObject = new CellProperties[DIMENSION][DIMENSION];
+    private static final String MINE = "\uD83D\uDCA3";
+    private static final String FLAG = "\uD83D\uDEA9";
+
     @Override
     public void initialize() {
         setScreenSize(DIMENSION, DIMENSION);
@@ -13,15 +16,26 @@ public CellProperties[][] cellProperties = new CellProperties[DIMENSION][DIMENSI
         for (int x = 0; x < DIMENSION; x++){
             for (int y = 0; y < DIMENSION; y++){
                 boolean isMine = getRandomNumber(100)<10;
-                cellProperties[x][y] = new CellProperties(x,y,isMine);
+                cellObject[x][y] = new CellProperties(x,y,isMine);
                 setCellColor(x,y, Color.BLANCHEDALMOND);
             }
         }
     }
 
-
-
-
-
-
+    @Override
+    public void onMouseLeftClick(int x, int y) {
+        openCell(x,y);
+    }
+    @Override
+    public void onMouseRightClick(int x, int y) {
+        //
+    }
+    private void openCell(int x, int y){
+        CellProperties cellProperties = cellObject[x][y];
+        if (cellProperties.isMine){
+            setCellValue(cellProperties.x,cellProperties.y,MINE);
+        } else {
+            setCellColor(cellProperties.x,cellProperties.y,Color.CYAN);
+        }
+    }
 }
