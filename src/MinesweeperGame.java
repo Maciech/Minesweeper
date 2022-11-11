ceb5463b68b902e0bdf8cefc5ec12178da038a2a
Maciej Chodacki <maciek.chodacki@wp.pl>
2022-11-11 23:22:41 +0100

    First commit, start up of a panel without any parameters

---
+++ src/MinesweeperGame.java
@@ -0,0 +1,19 @@
+import com.codegym.engine.cell.Game;
+
+public class MinesweeperGame extends Game{
+private final int DIMENSION = 12;
+    @Override
+    public void initialize() {
+        setScreenSize(DIMENSION, DIMENSION);
+        createGame();
+    }
+    public void createGame(){
+
+    }
+
+
+
+
+
+
+}
